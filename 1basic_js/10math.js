2c058aa16f1aae6aa9bd27c2ed83c71f05b15a67
Rahulshaw58 <161324665+Rahulshaw58@users.noreply.github.com>
2024-07-08 11:45:50 +0000

    math

---
+++ 1basic_js/10math.js
@@ -0,0 +1,23 @@
+
+
+// console.log(Math);
+// console.log(Math.abs(5)); //Output:- 5
+// console.log(Math.abs(-5)); //Output:- 5
+
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.5)); // Basically uses is Math.round() method if 4.5>4.4,..,4.1, the print 5. otherwise 4 consider.
+
+// console.log(Math.ceil(4.2)); // if any number in point if you used Math.ceil() method. if number is precision then print increase in one in point left side digit. like 4.2 -> 5.
+// console.log(Math.floor(4.6)); // 4.8 0r etc ->4.
+
+// console.log(Math.sqrt(4)); //√4 == 2
+// console.log(Math.pow(2,4)); //2^4 = 16
+
+// find maximum & minimum number in series using Math.min() & Math.min() method.
+// console.log(Math.min(4,3,56,4,6,3,2)); // 2
+// console.log(Math.max(4,3,56,4,6,3,2)); // 56
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
--- 1basic_js/9num_math.js
+++ 1basic_js/9num_math.js
@@ -1,16 +1,29 @@
 const score =400;
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
 //Convert in string using toString() method 
 //Check lenght of number using toString().length method.
-console.log(balance.toString().length);
+// console.log(balance.toString().length);
 
 //Precision values control
-console.log(balance.toFixed(2));
+// console.log(balance.toFixed(2));
+ 
+// precision roundup value using toPrecision(Total digit of number) method.
+// 
+const num1=4577.69842;
 
-const num1=4576.94842;
+// console.log(num1.toPrecision(4)); // output:- 4578
 
-console.log(num1.toPrecision(4));
+// large number aarangement by default in us.
+
+const num= 1000000;
+console.log(num.toLocaleString());
+// output:- 1,000,000
+
+// Large number aarange in Indian.
+const num2= 1000000;
+console.log(num.toLocaleString('en-IN'));
+
