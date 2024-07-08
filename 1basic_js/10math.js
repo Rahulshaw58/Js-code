046e3de0aad835eb88a6f80e35874fc427ea3767
Rahulshaw58 <161324665+Rahulshaw58@users.noreply.github.com>
2024-07-08 11:58:27 +0000

    math

--- 1basic_js/10math.js
+++ 1basic_js/10math.js
@@ -21,3 +21,7 @@ console.log(Math.random());
 console.log((Math.random()*10)+1);
 console.log(Math.floor((Math.random()*10)+1));
 
+const min=100000;
+const max=999999;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
