525101079ac2d5df91368a33cc00eb80c4525522
Rahulshaw58 <161324665+Rahulshaw58@users.noreply.github.com>
2024-07-12 12:42:45 +0000

    array2

--- alljs/13array2.js
+++ alljs/13array2.js
@@ -1,7 +1,7 @@
 const bhojpuri =["pawan","kheshari","Nilkamal"]
 const bollywod =["SunnyDeol","Ajay",]
 
-bhojpuri.push(bollywod)
+// bhojpuri.push(bollywod)
 // console.log(bhojpuri);
 // [ 'pawan', 'kheshari', 'Nilkamal', [ 'SunnyDeol', 'Ajay' ] ]
 
@@ -12,31 +12,27 @@ bhojpuri.push(bollywod)
 // console.log(bhojpuri);
 //[ 'pawan', 'kheshari', 'Nilkamal', [ 'SunnyDeol', 'Ajay' ] ]
 // console.log(bhojpuri[3][1]);
+
+//Seprate array not show cost.
+// bhojpuri.concat(bollywod);
 // const allheros = bhojpuri.concat(bollywod);
-// console.log(allHeros);
-//[
-//     'pawan',
-//     'kheshari',
-//     'Nilkamal',
-//     [ 'SunnyDeol', 'Ajay' ],
-//     'SunnyDeol',
-//     'Ajay'
-//   ]
+// console.log(allheros);
+//Output:-
+//[ 'pawan', 'kheshari', 'Nilkamal', 'SunnyDeol', 'Ajay' ]
 
-const all_new_heros = [...bhojpuri, ...bollywod]
-
+//*.spread 
+const all_new_heros = [...bhojpuri, ...bollywod];
 // console.log(all_new_heros);
-
-// [
-//     'pawan',
-//     'kheshari',
-//     'Nilkamal',
-//     [ 'SunnyDeol', 'Ajay' ],
-//     'SunnyDeol',
-//     'Ajay'
-//   ]
+//Output:-
+//[ 'pawan', 'kheshari', 'Nilkamal', 'SunnyDeol', 'Ajay' ]
 
 
 const another_array = [1, 2, 3, [4,5, 6], 7, [6, 7, [4, 5]]];
 
-
+const real= another_array.flat(Infinity);
+console.log(real);
+// [
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]
